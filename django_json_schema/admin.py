68c576694123c0156b7fc7618d6ad8b5a162644b
Floris272 <floris@maykinmedia.nl>
2025-01-09 14:58:29 +0100

    Add IndentedJSONEncoder

--- django_json_schema/admin.py
+++ django_json_schema/admin.py
@@ -1,8 +1,24 @@
+import json
+
 from django.contrib import admin
+from django import forms
 
 from .models import JsonSchema
+
+class IndentedJSONEncoder(json.JSONEncoder):
+    def __init__(self, *args, indent, sort_keys, **kwargs):
+        super().__init__(*args, indent=2, **kwargs)
+
+
+class JsonSchemaAdminForm(forms.ModelForm):
+    schema = forms.JSONField(encoder=IndentedJSONEncoder)
+
+    class Meta:
+        model = JsonSchema
+        fields = '__all__'
 
 
 @admin.register(JsonSchema)
 class JsonSchemaAdmin(admin.ModelAdmin):
+    form = JsonSchemaAdminForm
     search_fields = ["name"]
